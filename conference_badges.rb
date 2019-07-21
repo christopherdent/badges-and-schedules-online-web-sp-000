41af9e4a8edec8ed4946ff7435bf83e8089631ab
Christopher Dent <christopherdent01@gmail.com>
2019-07-21 21:48:34 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -39,10 +39,12 @@ end
 
 def printer(speakers)
   
-batch_badge_creator(speakers).each do
+batch_badge_creator(speakers).each do |badge|
+  puts badge
 end 
 
-assign_rooms(speakers).each do
+assign_rooms(speakers).each do |room|
+  puts room 
 end 
 
 
