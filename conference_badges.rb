6832908df31a0e2e8776afdd42a94a8381c55e1c
Christopher Dent <christopherdent01@gmail.com>
2019-07-21 21:27:11 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,40 @@
-# Write your code here.
+def badge_maker(name)
+  p "Hello, my name is #{name}."
+  
+end 
+
+
+######################################################
+
+
+speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
+def batch_badge_creator(speakers)
+      badges = []
+      
+      speakers.each do |speaker|
+      badges.push("Hello, my name is #{speaker}.")
+      end 
+      p badges
+end
+
+############################################################
+
+def assign_rooms(speakers)
+  rooms = []
+  counter = 1
+  if counter < 7
+    speakers.each do |speaker|
+      rooms.push("Hello, #{speaker}! You'll be assigned to room #{counter}!")
+      counter += 1
+     end 
+  end 
+  p rooms 
+end 
+
+def printer(speakers)
+  puts batch_badge_creator(speakers)
+  puts assign_rooms(speakers)
+end 
+
+ 
